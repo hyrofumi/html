4ca555cf9f81d65ed0ec712a331d6ef20c87b7e1
hyrofumi <leohrsagittarius+bitbucket@gmail.com>
2015-12-16 14:19:38 +0900

    resourceにリネームしたので削除

--- src/gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-
-gulp.task( "watch", function(){
-  var _ = require("../config.js");
-  var $ = require('gulp-load-plugins')();
-
-  gulp.watch(_.compass.src, ["compass"]);
-} );
